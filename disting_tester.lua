4c6717ac0a7bfcc438025635f2ca252c9756b779
Bram <7410845+stolmine@users.noreply.github.com>
2024-12-13 00:28:25 -0600

    still trying to figure out why DEX won't trigger, going to bed to diffuse.

--- disting_tester.lua
+++ disting_tester.lua
@@ -1,41 +1,50 @@
-
 --- disting ex tester
 -- test this shit out
 
 function init()
-    ii.disting.algorithm(21)
+    ii.disting.algorithm(21)     -- Set algorithm to Macro Oscillator 2
+    ii.disting.note_pitch(1, 60)   -- Trigger voice 1 with MIDI pitch 60 (Middle C)
+    print("Sent voice_on command: Voice 1, Pitch 60")
 end
 
-seq2 = sequins.new{'-' , ' ', ' ' , '+', ' ', '-', '/', '|' , '.' , '-' , '/' , '+', ' ', ' ', '|', ' ', '.', '/'}
-seq = sequins.new{'+' , ' ' , seq2 , '-', ' ',  '.' , '+' , '|' , '-'} -- A simple major scale pattern (in semitones)
-static_seq = sequins.new{2.5, 3.0, 3.5, 4.0, 4.5, -3, -4.5, 2, -1.35, 2.95} -- Static voltages
+-- function init()
+--     ii.disting.algorithm(21)
+-- end
+
+-- seq2 = sequins.new{'-' , ' ', ' ' , '+', ' ', '-', '/', '|' , '.' , '-' , '/' , '+', ' ', ' ', '|', ' ', '.', '/'}
+-- seq = sequins.new{'+' , ' ' , seq2 , '-', ' ',  '.' , '+' , '|' , '-'} -- A simple major scale pattern (in semitones)
+-- static_seq = sequins.new{2.5, 3.0, 3.5, 4.0, 4.5, -3, -4.5, 2, -1.35, 2.95} -- Static voltages
 
 
-function make_sound(char)
-    if     char == '+' then 
-        ii.disting.parameter( 7 , 6 )
-        ii.disting.voice_on( 1, 100)
-    elseif char == '|' then 
-        ii.disting.parameter( 7 , 2 )
-    elseif char == '.' then 
-        ii.disting.voice_off( 1, 0 )
-    elseif char == '/' then
-        static_value = static_seq()
-        output[1].volts = static_seq()
-    end -- note that spaces are ignored!
-  end
+-- function make_sound(char)
+--     if     char == '+' then 
+--         ii.disting.parameter( 7 , 6 )
+--         ii.disting.voice_on( 1, 16000)
+--         print("Disting: Voice On (nil), Param 7 = 6")
+--     elseif char == '|' then 
+--         ii.disting.parameter( 7 , 2 )
+--         print("Disting: Param 7 = 2")
+--     elseif char == '.' then 
+--         ii.disting.voice_off( 1 )
+--         print("Disting: Voice off")
+--     elseif char == '/' then
+--         static_value = static_seq()
+--         output[1].volts = static_seq()
+--     end -- note that spaces are ignored!
+--   end
 
--- Define the metro
-m = metro.init()
+-- -- Define the metro
+-- m = metro.init()
 
--- Configure the metro's event
-m.event = function()
-    local char = seq()  -- Step through the sequence
-    make_sound(char)    -- Call the function to trigger sound
-end
+-- -- Configure the metro's event
+-- m.event = function()
+--     local char = seq()  -- Step through the sequence
+--     make_sound(char)    -- Call the function to trigger sound
+--     print("char:", char, "static voltage:", static_value) -- Debug output
+-- end
 
--- Set metro parameters and start
-m.time = 0.025 -- Set the interval (0.5 seconds here)
-m.count = -1   -- Infinite repeats
-m:start()      -- Start the metro
-m:stop()       -- and stop it
+-- -- Set metro parameters and start
+-- m.time = 0.025 -- Set the interval (0.5 seconds here)
+-- m.count = -1   -- Infinite repeats
+-- m:start()      -- Start the metro
+-- m:stop()       -- and stop it
