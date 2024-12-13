36824e60ab6bb0d1180aa68440561c06a5910776
Bram <7410845+stolmine@users.noreply.github.com>
2024-12-12 23:08:36 -0600

    adding static values to the 301 sample selector instead of randomness

--- sequins_study.lua
+++ sequins_study.lua
@@ -2,6 +2,7 @@
 -- fuckin about
 seq2 = sequins.new{'-' , ' ', ' ' , '|' , '.' , '-'}
 seq = sequins.new{'+' , ' ' , seq2 ,  '.' , '+' , '|' , '-'} -- A simple major scale pattern (in semitones)
+static_seq = sequins.new{2.5, 3.0, 3.5, 4.0, 4.5, -3, -4.5, 2, -1.35, 2.95} -- Static voltages
 
 
 function make_sound(char)
@@ -9,8 +10,11 @@ function make_sound(char)
     elseif char == '|' then ii.jf.trigger( 4, 1 )
     elseif char == '.' then ii.jf.trigger( 3, 1 )
     elseif char == '-' then 
-        ii.er301.cv( 1, rand ) -- use the updated value for rand
-        ii.er301.tr_pulse( 2, 1 )
+        if static_seq:peek() then
+            local static_value = static_seq()  -- Get a static voltage from the sequence
+            ii.er301.cv(2, static_value)       -- Send static voltage to ER-301 on channel 2
+            ii.er301.tr_pulse(2, 1)              -- Trigger pulse
+        end
     end -- note that spaces are ignored!
   end
 
@@ -19,14 +23,13 @@ m = metro.init()
 
 -- Configure the metro's event
 m.event = function()
-    rand = math.random() * 10 - 5
     local char = seq()  -- Step through the sequence
     make_sound(char)    -- Call the function to trigger sound
-    print("char:", char, "rand:", rand) -- Debug output
+    print("char:", char, "static voltage:", static_value) -- Debug output
 end
 
 -- Set metro parameters and start
-m.time = 0.025                          -- Set the interval (0.5 seconds here)
-m.count = -1                          -- Infinite repeats
-m:start()                             -- Start the metro
-m:stop() -- and stop it
+m.time = 0.025 -- Set the interval (0.5 seconds here)
+m.count = -1   -- Infinite repeats
+m:start()      -- Start the metro
+m:stop()       -- and stop it
