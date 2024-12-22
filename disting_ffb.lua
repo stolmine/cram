554c91a61e3dedd5e3c4248c7ee8ac493244a1d8
Bram <7410845+stolmine@users.noreply.github.com>
2024-12-22 02:28:08 -0600

    added 301 melodic controls

--- disting_ffb.lua
+++ disting_ffb.lua
@@ -14,6 +14,7 @@ function init()
     dex.parameter(9, -40) -- set dry gain to negative infinity
     dex.parameter(10, -10) -- set wet gain to -10db
     dex.parameter(7, 1) -- set filter mode to BPF
+    od.tr_time(1, 500)
 
     local num = 21 -- set all gate params to 1 so we actually process audio when the script loads
     while num <= 28 do
@@ -26,6 +27,7 @@ function init()
     output[1].action = pulse() -- set output[1] to act as a generic pulse, we call this later via output[1]()
     output[2].action = pulse() -- ditto for output[2]
     output[3].action = pulse() -- and output 3
+    output[4].action = pulse()
 
     -- Generate parameter mappings dynamically, call this in repl to do it live!
     param_mappings = generate_param_mappings()
@@ -38,6 +40,11 @@ function init()
         end
     end
 end
+
+
+function sleep(n)
+    os.execute("sleep " .. tonumber(n))
+  end
 
 -- example usage
 
@@ -94,6 +101,46 @@ function send_gates(char) -- trying to send gates to txo, works manually but not
         txo.tr_pulse(3)
     elseif char == '$' then
         txo.tr_pulse(4)
+    end
+end
+
+function additive(char) -- adding in harmonic elements from the 301
+    print("additive function triggered with char:", char)
+    if char == '^' then
+        -- od.cv(1, 0)
+        -- sleep(0.01)
+        od.tr_pulse(1)
+        print("additive trig")
+    elseif char == '+' then
+        od.tr_pulse(1)
+    elseif char == '$' then
+        od.tr_pulse(1)
+    end
+end
+
+function od_param(char) -- adding in harmonic elements from the 301
+    print("od_param function triggered with char:", char)
+    if char == '+' then
+        od.cv(1, 0)
+        od.cv(2, 0)
+        od.tr_time(1, 100)
+    elseif char == '-' then
+        od.cv_slew(1, 500)
+        od.cv(1, 0.58333)
+        od.cv(2, 2.5)
+        od.tr_time(1, 500)
+    elseif char == '/' then
+        od.cv_slew(1, 0)
+        od.cv(1, -0.5)
+        od.cv(2, 4.5)
+        od.tr_time(250)
+    elseif char == '$' then
+        od.cv(1, 1)
+        od.cv(2, 6)
+        od.tr_time(1, 600)
+    elseif char == '^' then
+        pd.cv(1, -1)
+        od.cv(2, 0)
     end
 end
 
@@ -154,15 +201,45 @@ m3 = metro.init()
     m3:start()      -- Start the metro
     m3:stop()       -- and stop it
 
+m4 = metro.init()
+
+    m4.event = function()
+        local char = seq()
+        print("m4 triggered with seq value:", char)
+        additive(char)
+    end
+
+    m4.time = 0.15
+    m4.count = -1
+    m4:start()
+    m4:stop()
+
+m5 = metro.init()
+
+    m5.event = function()
+        local char = seq()
+        print("m5 triggered with seq value:", char)
+        od_param(char)
+    end
+
+    m5.time = 0.15
+    m5.count = -1
+    m5:start()
+    m5:stop()
+
 -- global stop and start functions for our metros
 function start()
     m:start()
     m2:start()
     m3:start() 
+    m4:start()
+    m5:start()
 end
 
 function stop()
     m:stop()
     m2:stop()
     m3:stop()
+    m4:stop()
+    m5:stop()
 end
