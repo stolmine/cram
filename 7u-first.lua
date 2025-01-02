3640ba9301cb8cd161d86ed1b170a00e396d2fd2
Bram <7410845+stolmine@users.noreply.github.com>
2025-01-01 22:04:21 -0600

    first for this new script, 7u-first.lua

---
+++ 7u-first.lua
@@ -0,0 +1,9 @@
+--- Just working on this first sketch for the new 7u setup
+-- a sequence for zaps to start
+
+function init()
+    dex = ii.disting
+    od = ii.er301
+    txo = ii.txo
+    txi = ii.txi
+end
