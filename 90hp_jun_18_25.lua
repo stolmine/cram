9b792b4ee7e87e8299e3c209188f28ab200d8279
Bram <7410845+stolmine@users.noreply.github.com>
2025-06-18 16:29:20 -0500

    initial commit for a blank canvas

---
+++ 90hp_jun_18_25.lua
@@ -0,0 +1,12 @@
+--- getting back into it
+-- sequencing, that's all
+
+function init()
+    txo = ii.txo
+    txi = ii.txi
+    dex = ii.disting
+    od = ii.er301
+    jf = ii.jf
+end
+
+-- characters for our sequins
